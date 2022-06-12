30fb9f334ba2fbd5bbbb187bbd0ce9c39a48d784
oppian1 <107006746+oppian1@users.noreply.github.com>
2022-06-12 21:17:47 +1200

    Update pass_gen_gui.py

--- pass_gen_gui.py
+++ pass_gen_gui.py
@@ -13,9 +13,9 @@ class PasswordGui(object):
     def __init__(self, parent, pass_length, characters):
         self.pass_length = pass_length
         self.characters = characters
-        self.button = Button(parent, text='Generate', command=self.update_label)
+        self.button = Button(parent, text='Generate Passowrd', command=self.update_label)
         self.button.grid(row=0, column=1, padx=50)
-        self.output = Label(parent, text='Press Generate -- Board man gets paid')
+        self.output = Label(parent, text='Press Generate')
         self.output.grid(row=1, column=1, padx=10, pady=10)
     
     def pass_generator(self, size, characters):
@@ -31,8 +31,7 @@ def input_length():
         user_input = input('How many characters? ')
     return int(user_input)
 
-def main():
-    """SWAGGGGG MONEY"""    
+def main():   
     window = Tk()
     length = input_length()
     characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqrRsStTuUvVwWxXyYzZ1234567890!@#$%^&*()~:"<>?{}|_+-=[]\;,./'
